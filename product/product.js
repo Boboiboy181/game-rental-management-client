ce4c96b6ba08779733e65f220449a34de5cc6662
hgmjkhug <yeuthaoto@gmail.com>
2023-06-02 14:11:00 +0700

    change branch

--- product/product.js
+++ product/product.js
@@ -52,77 +52,106 @@ function addProduct(event) {
 
 // Function to display all products in a table
 function displayProducts() {
-    const productTableBody = document.getElementById('product-table-body');
-    productTableBody.innerHTML = ''; // Clear previous content
-  
-    products.forEach(function (product, index) {
-      const row = document.createElement('tr');
-  
-      const sttCell = document.createElement('td');
-      sttCell.textContent = index + 1; // Add 1 to index for the serial number
-      row.appendChild(sttCell);
-  
-      const productNameCell = document.createElement('td');
-      productNameCell.textContent = product.productName;
-      row.appendChild(productNameCell);
-  
-      const productQuantityCell = document.createElement('td');
-      productQuantityCell.textContent = product.productQuantity;
-      row.appendChild(productQuantityCell);
-  
-      const productImportDateCell = document.createElement('td');
-      productImportDateCell.textContent = product.productImportDate;
-      row.appendChild(productImportDateCell);
-  
-      const productPriceCell = document.createElement('td');
-      productPriceCell.textContent = product.productPrice;
-      row.appendChild(productPriceCell);
-  
-      const productManufacturerCell = document.createElement('td');
-      productManufacturerCell.textContent = product.productManufacturer;
-      row.appendChild(productManufacturerCell);
-  
-      const productCategoryCell = document.createElement('td');
-      productCategoryCell.textContent = product.productCategory;
-      row.appendChild(productCategoryCell);
-  
-      const productReleaseDateCell = document.createElement('td');
-      productReleaseDateCell.textContent = product.productReleaseDate;
-      row.appendChild(productReleaseDateCell);
-  
-      const productLanguageCell = document.createElement('td');
-      productLanguageCell.textContent = product.productLanguage;
-      row.appendChild(productLanguageCell);
-  
-      const productPlatformCell = document.createElement('td');
-      productPlatformCell.textContent = product.productPlatform;
-      row.appendChild(productPlatformCell);
-  
-      const productDescriptionCell = document.createElement('td');
-      productDescriptionCell.textContent = product.productDescription;
-      row.appendChild(productDescriptionCell);
-  
-      const actionsCell = document.createElement('td');
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = 'Xóa';
-      deleteButton.addEventListener('click', function () {
-        showConfirmationDialog('delete', index);
-      });
-      actionsCell.appendChild(deleteButton);
-  
-      const updateButton = document.createElement('button');
-      updateButton.textContent = 'Cập nhật';
-      updateButton.addEventListener('click', function () {
-        showConfirmationDialog('update', index);
-      });
-      actionsCell.appendChild(updateButton);
-  
-      row.appendChild(actionsCell);
-  
-      productTableBody.appendChild(row);
+  const productTableBody = document.getElementById('product-table-body');
+  productTableBody.innerHTML = ''; // Clear previous content
+
+  products.forEach(function (product, index) {
+    const row = document.createElement('tr');
+
+    const sttCell = document.createElement('td');
+    sttCell.textContent = index + 1; // Add 1 to index for the serial number
+    row.appendChild(sttCell);
+
+    const productNameCell = document.createElement('td');
+    productNameCell.textContent = product.productName;
+    row.appendChild(productNameCell);
+
+    const productQuantityCell = document.createElement('td');
+    productQuantityCell.textContent = product.productQuantity;
+    row.appendChild(productQuantityCell);
+
+    const productImportDateCell = document.createElement('td');
+    productImportDateCell.textContent = product.productImportDate;
+    row.appendChild(productImportDateCell);
+
+    const productPriceCell = document.createElement('td');
+    productPriceCell.textContent = product.productPrice;
+    row.appendChild(productPriceCell);
+
+    const productManufacturerCell = document.createElement('td');
+    productManufacturerCell.textContent = product.productManufacturer;
+    row.appendChild(productManufacturerCell);
+
+    const productCategoryCell = document.createElement('td');
+    productCategoryCell.textContent = product.productCategory;
+    row.appendChild(productCategoryCell);
+
+    const productReleaseDateCell = document.createElement('td');
+    productReleaseDateCell.textContent = product.productReleaseDate;
+    row.appendChild(productReleaseDateCell);
+
+    const productLanguageCell = document.createElement('td');
+    productLanguageCell.textContent = product.productLanguage;
+    row.appendChild(productLanguageCell);
+
+    const productPlatformCell = document.createElement('td');
+    productPlatformCell.textContent = product.productPlatform;
+    row.appendChild(productPlatformCell);
+
+    const productDescriptionCell = document.createElement('td');
+    productDescriptionCell.textContent = product.productDescription;
+    row.appendChild(productDescriptionCell);
+
+    const actionsCell = document.createElement('td');
+    const detailButton = document.createElement('button');
+    detailButton.textContent = 'Chi tiết';
+    detailButton.addEventListener('click', function () {
+      showProductDetails(index);
     });
-  }
-  
+    actionsCell.appendChild(detailButton);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Xóa';
+    deleteButton.addEventListener('click', function () {
+      showConfirmationDialog('delete', index);
+    });
+    actionsCell.appendChild(deleteButton);
+
+    const updateButton = document.createElement('button');
+    updateButton.textContent = 'Cập nhật';
+    updateButton.addEventListener('click', function () {
+      showConfirmationDialog('update', index);
+    });
+    actionsCell.appendChild(updateButton);
+
+    row.appendChild(actionsCell);
+
+    productTableBody.appendChild(row);
+  });
+}
+
+// Function to show the detailed information of a product
+function showProductDetails(index) {
+  const product = products[index];
+
+  // Create a new element to display the additional information
+  const detailDiv = document.createElement('div');
+
+  // Add the necessary HTML content for the additional information
+  detailDiv.innerHTML = `
+    <p>Manufacturer: ${product.productManufacturer}</p>
+    <p>Category: ${product.productCategory}</p>
+    <p>Language: ${product.productLanguage}</p>
+    <p>Platform: ${product.productPlatform}</p>
+    <p>Description: ${product.productDescription}</p>
+  `;
+
+  // Append the detailDiv to the row in the table
+  const productTableBody = document.getElementById('product-table-body');
+  const row = productTableBody.children[index];
+  const actionsCell = row.lastElementChild;
+  row.insertBefore(detailDiv, actionsCell);
+}
 
 // Function to show the confirmation dialog
 function showConfirmationDialog(action, index) {
