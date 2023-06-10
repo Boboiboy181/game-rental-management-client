eb1adb4cd574c98c9d8f20447a030c65166d61f6
Gia Hai <haidao549@gmail.com>
2023-06-10 08:48:30 +0700

    setting up prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 const App = () => {
   return (
-    <div>
+    <div className="h-full flex justify-center align-baseline">
       <h1>Hello World</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
