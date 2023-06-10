329fb5dc1e035c8f27ce46835ab1a3349b6070ad
Gia Hai <haidao549@gmail.com>
2023-06-10 10:05:39 +0700

    version 1 of navigation

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,9 +1,28 @@
 import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <Fragment>
-      <div className="navigation"></div>
+      <div className="h-20 flex justify-around items-center border-solid border-gray-300 border-b">
+        <input
+          className="rounded-md border-2  bg-white px-2 py-1 focus:outline-none focus:border-indigo-500"
+          type="search"
+          placeholder="Search game"
+        />
+        <Link className="" to="/">
+          <img
+            className="h-12 w-12"
+            src="https://tailwindui.com/img/logos/mark.svg?color=black"
+            alt="Tailwind CSS Logo"
+          />
+        </Link>
+        <Link className="" to="/checkout">
+          <button className="rounded-md bg-blue-500 text-white h-9 w-24 hover:bg-red-500">
+            Checkout
+          </button>
+        </Link>
+      </div>
     </Fragment>
   );
 };
