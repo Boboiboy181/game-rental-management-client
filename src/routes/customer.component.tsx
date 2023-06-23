2805df8ff73f08c27de904877eaa2f6a5a257145
Doan Anh Hien <21520227@gm.uit.edu.vn>
2023-06-23 22:38:41 +0700

    customer feat

---
+++ src/routes/customer.component.tsx
@@ -0,0 +1,113 @@
+import { Space, Typography, Divider, Button } from 'antd';
+import Table from 'antd/es/table';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const { Text } = Typography;
+
+// // rowSelection object indicates the need for row selection
+// const rowSelection = {
+//   onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
+//     console.log(
+//       `selectedRowKeys: ${selectedRowKeys}`,
+//       'selectedRows: ',
+//       selectedRows,
+//     );
+//   },
+// };
+
+const Customer = () => {
+  const [customers, setCustomers] = useState<Customer[]>([]);
+
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      const { data }: { data: Customer[] } = await axios.get(
+        'https://game-rental-management-app-yh3ve.ondigitalocean.app/video-game',
+      );
+      setCustomers(data);
+    };
+
+    fetchCustomers();
+  }, []);
+
+  const columns = [
+    {
+      title: 'Customer Name',
+      dataIndex: 'customerName',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+    },
+    {
+      title: 'Phone Number',
+      dataIndex: 'phoneNumber',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+    },
+    {
+      title: 'point',
+      dataIndex: 'point',
+    }
+  ];
+
+  const data = customers.map((customer) => ({
+    key: customer._id,
+    customerName: customer.productName,
+    email: customer.email,
+    phoneNumber: customer.phoneNumber,
+    address: customer.address,
+    point: customer.point,
+  }));
+
+  const [searchField, setSearchField] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.toLocaleLowerCase();
+    setSearchField(value);
+  };
+
+  return (
+    <div className="w-[1080px] bg-white rounded-md relative top-[30%] left-[50%] translate-x-[-50%] translate-y-[-30%] p-10">
+      <Space className="flex justify-between">
+        <Text className="text-2xl font-semibold">Customer</Text>
+        <div className="input-field">
+          <input
+            className="px-4"
+            type="search"
+            placeholder="Search game"
+            name="searchField"
+            value={searchField}
+            onChange={handleChange}
+          />
+          <label htmlFor="searchfield">Search customer</label>
+        </div>
+      </Space>
+      <div>
+        <Divider />
+        <Table
+          rowSelection={{
+            type: 'checkbox',
+          }}
+          columns={columns}
+          dataSource={data}
+        />
+      </div>
+      <Space direction="horizontal" className="relative top-[-9%]">
+        <Button type="primary" className="bg-blue-500">
+          Thêm
+        </Button>
+        <Button danger type="primary">
+          Xóa
+        </Button>
+        <Button type="primary" className="bg-green-600">
+          Sửa
+        </Button>
+      </Space>
+    </div>
+  );
+};
+
+export default Customer;
