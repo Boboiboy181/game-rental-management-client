baf517bbfa5c1486da54b32068f7f88c19af979d
Doan Anh Hien <21520227@gm.uit.edu.vn>
2023-06-28 16:43:43 +0700

    fix price

--- src/types/rental.type.ts
+++ src/types/rental.type.ts
@@ -1,8 +1,11 @@
-type Rental = {
+import { Customer } from './customer.type';
+import { Product } from "./product.type";
+
+export type Rental = {
     _id: string;
     customer: string;
     deposit: number;
     returnValue: number;
     returnState: string;
-    estimatePrice: number;
+    estimatedPrice: number;
   }
