4e06b7e239a41401a9ca4ec811b10e73a922dc2d
Gia Hai <haidao549@gmail.com>
2024-02-28 08:56:55 +0700

    fixed group number

--- src/routes/introduction.component.tsx
+++ src/routes/introduction.component.tsx
@@ -1,7 +1,7 @@
 const Introduction = () => {
   return (
     <div className={'flex flex-col justify-center items-center'}>
-      <p className="mt-4 font-cursive text-5xl mb-8">Group 4</p>
+      <p className="mt-4 font-cursive text-5xl mb-8">Group 6</p>
       <ul className="text-2xl text-center">
         <li>Trần Trúc Quỳnh</li>
         <li>Đào Gia Hải</li>
